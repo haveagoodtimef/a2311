89df4311ace2837447c8ea01249255711f96c739
feng <78236491@qq.com>
2026-07-01 10:27:04 +0800

    添加参数

--- app/src/main/java/com/fenghongzhang/anew/Feng.kt
+++ app/src/main/java/com/fenghongzhang/anew/Feng.kt
@@ -3,6 +3,7 @@ package com.fenghongzhang.anew
 @Target(AnnotationTarget.FUNCTION,AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class Feng(
-    val value: String
+    val value: String,
+    val haha: String
 )
 
